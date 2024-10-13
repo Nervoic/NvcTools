708928a09715127edbedea3cf0389b99d41b99ed
Nervoic <nearveanee@gmail.com>
2024-10-13 21:46:47 +0300

    Update

--- NvcTools/Instance.cs
+++ NvcTools/Instance.cs
@@ -24,6 +24,16 @@ public static class Instance
         return InitGetComponent<T>(obj, addIfNull);
     }
 
+        /// <summary>
+    /// Initialize instance
+    /// </summary>
+    /// <typeparam name="T">Component to initialize</typeparam>
+    /// <param name="obj">Your gameobject</param>
+    /// <returns>Return initialized value</returns>
+    public static T[] InitsGet<T>(GameObject obj) where T : Component {
+        return InitsGetComponent<T>(obj);
+    }
+
     /// <summary>
     /// Initialize instance, if component is available in scene
     /// </summary>
@@ -33,6 +43,15 @@ public static class Instance
         return InitFindComponent<T>();
     }
 
+    /// <summary>
+    /// Initialize instance, if components is available in scene
+    /// </summary>
+    /// <typeparam name="T">Components to initialize</typeparam>
+    /// <returns>Return initialized values</returns>
+    public static T[] InitsFind<T>() where T : Component {
+        return InitsFindComponent<T>();
+    }
+ 
     private static T InitGetComponent<T>(GameObject obj, bool addIfNull) where T : Component {
         try {
             T component = obj.GetComponent<T>();
@@ -50,10 +69,38 @@ public static class Instance
         }
     }
 
+    private static T[] InitsGetComponent<T>(GameObject obj) where T : Component {
+        try {
+            T[] components = obj.GetComponents<T>();
+            if(components.Length != 0 && components != null) return components;
+            else {
+                NullLog<T>();
+                return default;
+            }
+        } catch(Exception e) {
+            Debug.Log(e.Message);
+            return default;
+        }
+    }
+
     private static T InitFindComponent<T>() where T : Component {
         try {
             T component = GameObject.FindObjectOfType<T>();
             if(component != null) return component;
+            else {
+                NullLog<T>();
+                return default;
+            }
+        } catch(Exception e) {
+            Debug.Log(e.Message);
+            return default;
+        }
+    }
+
+    private static T[] InitsFindComponent<T>() where T : Component {
+        try {
+            T[] components = GameObject.FindObjectsOfType<T>();
+            if(components.Length != 0 && components != null) return components;
             else {
                 NullLog<T>();
                 return default;
